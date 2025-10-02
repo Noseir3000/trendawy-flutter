e4d0e1b4713699517e210b90deae4b2431f8a38e
Noseir3000 <noseir3000@gmail.com>
2025-10-02 18:13:51 +0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TrendawyApp());
+}
+
+class TrendawyApp extends StatelessWidget {
+  const TrendawyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Trendawy',
+      theme: ThemeData.dark(),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Hello, Trendawy! 🚀'),
+        ),
+      ),
+    );
+  }
+}
