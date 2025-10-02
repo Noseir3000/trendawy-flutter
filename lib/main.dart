6be449416b8f3b722d2b9942eb2cb9e73a76ec3a
Noseir3000 <noseir3000@gmail.com>
2025-10-02 18:46:18 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class TrendawyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Trendawy',
+      title: 'Trendawy Flutter',
       theme: ThemeData.dark(),
       home: const Scaffold(
         body: Center(
